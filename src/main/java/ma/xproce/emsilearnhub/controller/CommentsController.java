6a2024a5d9b2e6cf8cb42791c53a3a0ca70804a0
Mohammed_Pc <mohammed_pc@asus.com>
2024-04-12 16:12:51 +0000

    added comment components

---
+++ src/main/java/ma/xproce/emsilearnhub/controller/CommentsController.java
@@ -0,0 +1,38 @@
+package ma.xproce.emsilearnhub.controller;
+
+import lombok.AllArgsConstructor;
+import ma.xproce.emsilearnhub.dto.CommentsDto;
+import ma.xproce.emsilearnhub.service.CommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static javax.security.auth.callback.ConfirmationCallback.OK;
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/api/comments")
+@AllArgsConstructor
+public class CommentsController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
+        return new ResponseEntity<>(CREATED);
+    }
+
+    @GetMapping(params = "postId")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@RequestParam Long postId) {
+        return ResponseEntity.status(OK)
+                .body(commentService.getAllCommentsForPost(postId));
+    }
+
+    @GetMapping(params = "userName")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@RequestParam String userName){
+        return ResponseEntity.status(OK)
+                .body(commentService.getAllCommentsForUser(userName));
+    }
+
+}
---
+++ src/main/java/ma/xproce/emsilearnhub/mapper/CommentMapper.java
@@ -0,0 +1,23 @@
+package ma.xproce.emsilearnhub.mapper;
+
+import ma.xproce.emsilearnhub.dto.CommentsDto;
+import ma.xproce.emsilearnhub.model.Comment;
+import ma.xproce.emsilearnhub.model.Post;
+import ma.xproce.emsilearnhub.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    @Mapping(target = "user", source = "user")
+    Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentsDto mapToDto(Comment comment);
+}
+
